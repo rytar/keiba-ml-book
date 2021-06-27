b9fa20d251946e48c6a34cfb65b8f83d3a0c2892
rytaryu <53173674+rytar@users.noreply.github.com>
2021-06-27 14:48:08 +0900

    create scraper

---
+++ src/Scraper.py
@@ -0,0 +1,35 @@
+import requests
+import time
+import threading
+from tqdm import tqdm
+
+class Scraper:
+
+    def __init__(self, sleep_time=1):
+        self.__sleep_time = sleep_time
+
+    
+    def __get_html(self, url):
+        def func():
+            try:
+                res = requests.get(url)
+                res.raise_for_status()
+                self.htmls.append(res.content)
+            except Exception as e:
+                print(e)
+
+        return func
+        
+
+    def run(self, urls):
+        self.htmls = list()
+
+        base_time = time.time()
+        next_time = 0
+        for url in tqdm(urls):
+            t = threading.Thread(target=self.__get_html(url))
+            t.start()
+            next_time = ((base_time - time.time()) % self.__sleep_time) or self.__sleep_time
+            time.sleep(next_time)
+
+        return self.htmls
---
+++ src/get_data.py
@@ -0,0 +1,20 @@
+import itertools
+from src.Scraper import Scraper
+
+def main():
+    start_year = 2011
+    end_year = 2020
+    years = [ str(year) for year in range(start_year, end_year + 1) ]
+    codes = [ str(i + 1).zfill(2) for i in range(10) ]
+    race_counts = [ str(i + 1).zfill(2) for i in range(6) ]
+    days = [ str(i + 1).zfill(2) for i in range(8) ]
+    race_nums = [ str(i + 1).zfill(2) for i in range(12) ]
+    race_ids = list(itertools.product(years, codes, race_counts, days, race_nums))
+    race_ids = [ ''.join(race_id) for race_id in race_ids ]
+    urls = [ 'https://race.netkeiba.com/race/result.html?race_id={}'.format(race_id) for race_id in race_ids ]
+
+    scraper = Scraper()
+    htmls = scraper.run(urls)
+
+if __name__ == '__main__':
+    main()
